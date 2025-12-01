1bf37a36011da19511612eb2966e6e44d83db833
Sujit Mecwan <mecwansujit@gmail.com>
2025-12-01 18:04:22 +0530

    updated

--- Main.java
+++ Main.java
@@ -4,17 +4,34 @@ import java.util.SortedSet;
 public class Main {
     public static void main(String[] args) {
         //primitive datatypes
-        System.out.println("hello world");
-        byte age = 52;
-        int phone = 1234567891;
-        long number = 1213484314665454615L;
-        float pi = 3.14F;
-        char ch = 'a';
-        boolean isAdult = false;
+//        System.out.println("hello world");
+//        byte age = 52;
+//        int phone = 1234567891;
+//        long number = 1213484314665454615L;
+//        float pi = 3.14F;
+//        char ch = 'a';
+//        boolean isAdult = false;
 
         //nonprimitive datatypes
 
-        String name= "sujit";
-        System.out.println(name.length());
+//        String name= "sujit";
+//        System.out.println(name.length());
+
+
+        //concatenation
+//        String name1="Sujit ";
+//        String name2="Mecwan";
+//        String name3= name1+ name2;
+//        System.out.println(name3);
+
+        //charAt
+//        String name="sujit";
+//        System.out.println(name.charAt(2));
+
+
+        //replace
+//        String name="sujit";
+//        String updatedname = name.replace('j', 'm');
+//        System.out.println(updatedname);
     }
 }
