3ea6fd2ff44538fd68a791d78caa15204ebc2747
Sujit Mecwan <mecwansujit@gmail.com>
2025-12-01 18:49:06 +0530

    updated

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.sql.Array;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.SortedSet;
 
 public class Main {
@@ -33,5 +35,26 @@ public class Main {
 //        String name="sujit";
 //        String updatedname = name.replace('j', 'm');
 //        System.out.println(updatedname);
+
+        //substring
+//        String name="sujit mecwan";
+//        System.out.println(name.substring(0,5));
+
+        int[] marks = new int[5];
+
+        marks[0] = 59;
+        marks[1] = 77;
+        marks[2] = 89;
+        marks[3] = 51;
+        marks[4] = 52;
+
+        //length
+//        System.out.println(marks.length);
+
+        //sort
+        System.out.println(marks[0]);
+        Arrays.sort(marks);
+        System.out.println(marks[0]);
+
     }
 }
