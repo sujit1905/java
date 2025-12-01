fbd3d2d97717d42294e3f623fb2cb8ef77fc7713
Sujit Mecwan <mecwansujit@gmail.com>
2025-12-01 17:06:07 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.SortedSet;
+
+public class Main {
+    public static void main(String[] args) {
+        //primitive datatypes
+        System.out.println("hello world");
+        byte age = 52;
+        int phone = 1234567891;
+        long number = 1213484314665454615L;
+        float pi = 3.14F;
+        char ch = 'a';
+        boolean isAdult = false;
+
+        //nonprimitive datatypes
+
+        String name= "sujit";
+        System.out.println(name.length());
+    }
+}
