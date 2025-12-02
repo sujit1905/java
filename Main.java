d87be4e6b01b9e108deb0af08a32413d904c0e61
Sujit Mecwan <mecwansujit@gmail.com>
2025-12-02 18:10:52 +0530

    day02

--- Main.java
+++ Main.java
@@ -57,13 +57,42 @@ public class Main {
 
         //casting
             //double has 8bytes
-            double price=100.00;
-            double finalPrice=price+18.0;
+//            double price=100.00;
+//            double finalPrice=price+18.0;
             //int has 2 bytes
-//        int price=100;
-//        int finalPrice=price+18;
+/*        int price=100;
+        int finalPrice=price+(int)18.0;
+        System.out.println(finalPrice);*/
 
-        System.out.println(finalPrice);
+        //constants
+
+//        final float pi=3.14F;
+//        System.out.println(pi);
+
+
+        //oprators
+
+        //arithmatic oprators
+//        int a=10;
+//        int b=5;
+//        int c=a+b;
+//        int c=a-b;
+//        int c=a*b;
+//        int c=a/b;
+//        int c=a%b;
+//        System.out.println(c);
+
+//        increment and decrement operator
+
+//        int a=5;
+//        a++; postincrement
+//        ++a; preincrement
+
+//        a--; postdecrement
+//        --a;predecrement
+
+//        System.out.println(a);
+
 
     }
 }
